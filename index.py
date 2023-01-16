8c83dfb535d3d3ea48f3f749b57dfc054f47baff
KaonWry <titan2011official@gmail.com>
2023-01-17 05:49:00 +0700

    Change wait param, link fix
    Now waiting for the overlay page to load instead of fixed time
    
    I just realised that ive been using my own landing page (with NIM)
    instead of using the auth all along lmao

--- index.py
+++ index.py
@@ -2,6 +2,10 @@ import datetime
 import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import urllib.request
 from lxml import etree
 driver = webdriver.Firefox()
@@ -28,7 +32,7 @@ def remove_element(xpath):
     """, element)
 
 # Login
-driver.get('https://akademik.itb.ac.id/app/K/mahasiswa:16322294+2022-2/kelas/jadwal/mahasiswa')                                         # Got to web
+driver.get(f'https://akademik.itb.ac.id/app/K/mahasiswa:{nim}/kelas')                                         # Got to web
 driver.find_element('xpath', '/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/a').click()                                             # Click login button
 driver.find_element('xpath', '//*[@id="username"]').send_keys(nim)                                                                      # Send username
 print('Inputted username')
@@ -38,12 +42,20 @@ driver.find_element('xpath', '/html/body/div/div/div[2]/div/div[1]/div/div[2]/di
 if check_exists_by_xpath("//*[contains(text(), 'Jadwal Perkuliahan Mahasiswa')]"):
     print('Login successful')
     # Search for the lecture
-    hour = str(datetime.datetime.now().strftime('%H'))
+    hour = str(datetime.datetime.now().strftime('%H'))      # The real deal
+    # hour = str(7)                                         # Testing purposes
     lecture = f"{hour}:00-"
-    classes = driver.find_elements('xpath', f'//*[contains(text(), "{lecture}" )]')
-    for i in classes:
+    attend = driver.find_elements('xpath', f'//*[contains(text(), "{lecture}" )]')
+    # Bruteforce all classes in the same hour to search for that one attendance button
+    for i in attend:
         i.click()
-        time.sleep(2.5)
+        try:
+            element_present = EC.presence_of_element_located((By.XPATH , '/html/body/div[2]/div[2]/div/div/div/div/div/div/div/div[4]/button'))
+            WebDriverWait(driver, 3).until(element_present)
+        except TimeoutException:
+            print ("Loading took too much time!")
+            remove_element('/html/body/div[2]')
+            continue            
         if check_exists_by_xpath('//*[@id="form_hadir"]'):
             break
         else:
@@ -53,4 +65,3 @@ if check_exists_by_xpath("//*[contains(text(), 'Jadwal Perkuliahan Mahasiswa')]"
     driver.find_element('xpath', '//*[@id="form_hadir"]').click()
 else:
     print('Login failed')
-    
